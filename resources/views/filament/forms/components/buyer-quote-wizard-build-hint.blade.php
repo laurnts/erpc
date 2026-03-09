54f0bd7ca24bd29f2e9dc46e4e59d41b7d13f687
Rio Rubiarjo <sevenouthree@gmail.com>
2026-03-09 14:06:51 +0700

    remove unnecessary code

--- resources/views/filament/forms/components/buyer-quote-wizard-build-hint.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="rounded-lg border border-gray-200 bg-gray-50 p-3 dark:border-gray-600 dark:bg-gray-800">
-    <p class="text-sm text-gray-600 dark:text-gray-400">
-        This quote will be created from the selected supplier quote(s). Payment terms and line items will be copied. You can edit the quote after creation.
-    </p>
-</div>
--- resources/views/filament/forms/components/buyer-quote-wizard-load-step2.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- Load quote details only (no supplier_groups) to avoid 502; line items built on submit. --}}
-<div
-    x-data
-    x-init="$wire.call('loadWizardStep2Data')"
-    class="hidden"
-    aria-hidden="true"
-></div>
